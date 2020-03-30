5db9fb5d82fd5f8543f4f70495ccbd01c14e1def
rafaelgssa <rafael.gssa@gmail.com>
2020-03-30 11:06:16 -0300

    Add confirmation dialog

---
+++ src/app/shared/confirmation-dialog/confirmation-dialog.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface ConfirmationDialogData {
+  title: string,
+}
+
+@Component({
+  selector: 'confirmation-dialog',
+  templateUrl: 'confirmation-dialog.html',
+})
+export class ConfirmationDialog {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
