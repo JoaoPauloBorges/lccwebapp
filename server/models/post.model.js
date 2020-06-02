d91d9eec53d66a6fb8375bc4224222f816f5a8c5
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2020-06-01 23:22:33 -0300

    Delete images on 'findOneAndDelete'
    delete image files when mongoose pre hook findOneAndDelete is activated

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('../common/services/mongoose.service').mongoose;
-
 const Schema = mongoose.Schema;
+
+const fileUploadModel = require('./files-upload.model');
+
 
 const postSchema = Schema({
    author: String,
@@ -9,5 +11,15 @@ const postSchema = Schema({
    imagePath: String,
    content: String
 }, { versionKey: false });
+
+postSchema.pre('findOneAndDelete', async function () {
+  const docToUpdate = await this.model.findOne(this.getQuery());
+  console.log(docToUpdate); // The document that `findOneAndDelete()` will modify
+  fileUploadModel.gfs.remove({ filename: docToUpdate.imagePath, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+});
 
 module.exports = mongoose.model('Post', postSchema);
